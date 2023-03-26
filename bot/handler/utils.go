bfb31eba06650cdb7fda7f50da4eb64e538e263a
debugg-er <khainguyen9209@gmail.com>
2023-03-26 11:05:41 +0700

    update

--- bot/handler/utils.go
+++ bot/handler/utils.go
@@ -9,11 +9,13 @@ import (
 
 func sendTelegramMessage(c *bot.Context, content string) (telegram.Message, error) {
 	msg := telegram.NewMessage(c.Update.Message.Chat.ID, content)
+	msg.ParseMode = telegram.ModeMarkdown
 	return c.BotAPI.Send(msg)
 }
 
 func editTelegramMsg(c *bot.Context, msg telegram.Message, content string) (telegram.Message, error) {
 	editMsg := telegram.NewEditMessageText(msg.Chat.ID, msg.MessageID, content)
+	editMsg.ParseMode = telegram.ModeMarkdown
 	return c.BotAPI.Send(editMsg)
 }
 
